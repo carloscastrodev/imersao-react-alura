20e23550802113841e1ab0262aeb782f02d1b534
Carlos Vinicius <contato-carloscastro@hotmail.com>
2020-08-01 07:01:46 -0300

    Component to show and choose category on /newvideo

--- src/components/CategoryList/index.jsx
+++ src/components/CategoryList/index.jsx
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CategoryItem from './CategoryItem';
+import SearchBar from '../SearchBar';
+import useOnMount from '../hooks/onMount';
 import './styles.css';
+import NoItemsFilter from '../NoItemsFilter';
 
 const CategoryList = ({ categories, setCategory }) => {
+  const [filteredCategories, setFilteredCategories] = useState([]);
+
+  useOnMount(() => {
+    setFilteredCategories(categories);
+  });
+
   return (
     <div className="category-list-wrapper">
       <ul className="category-list">
-        {categories.map(category => (
-          <CategoryItem
-            key={category}
-            categoryName={category}
-            setCategory={setCategory}
+        <div className="search-bar">
+          {' '}
+          <SearchBar
+            placeholder="Nome do Anime"
+            all={categories}
+            setFiltered={setFilteredCategories}
           />
-        ))}
+        </div>
+        {(filteredCategories.length > 0 &&
+          filteredCategories.map(category => (
+            <CategoryItem
+              key={category}
+              categoryName={category}
+              setCategory={setCategory}
+            />
+          ))) || <NoItemsFilter />}
       </ul>
     </div>
   );
