a444097890fcbcfbfd246f9a2077e73bacb4b792
Carlos Vinicius <contato-carloscastro@hotmail.com>
2020-08-01 06:59:41 -0300

    New /manage route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import NewVideo from './pages/NewVideo';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Layout from './components/shared/Layout';
 import Page404 from './pages/Page404';
+import Manage from './pages/Manage';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Switch>
           <Route path="/" exact={true} component={Home} />
           <Route path="/newvideo" component={NewVideo} />
+          <Route path="/manage" component={Manage} />
           <Route path="*" component={Page404} />
         </Switch>
       </Layout>
