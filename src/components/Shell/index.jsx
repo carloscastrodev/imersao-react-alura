11d0fd3e82915ec0b24b7d59d8387dbe86189cb1
Carlos Vinicius <contato-carloscastro@hotmail.com>
2020-08-02 00:18:50 -0300

    Shell component refactor to better indicate load

--- src/components/Shell/index.jsx
+++ src/components/Shell/index.jsx
@@ -6,6 +6,17 @@ const Shell = ({ bg }) => {
     height: 100%;
     width: 100%;
     background-color: ${({ bg }) => bg || 'black'};
+    animation: pulsate 0.5s infinite;
+
+    @keyframes pulsate {
+      from {
+        opacity: 1;
+      }
+      to {
+        opacity: 0.1;
+        background-color: rgba(40, 40, 40, 0.5);
+      }
+    }
   `;
   return <StyledShell bg={bg} />;
 };
