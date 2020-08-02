f1ca66311b8a61771d5fef1d1b816006fb83bcb9
Carlos Vinicius <contato-carloscastro@hotmail.com>
2020-08-02 00:16:50 -0300

    Revert margin top to 0 and code refactor

--- src/components/HighlightVideoPlayer/index.jsx
+++ src/components/HighlightVideoPlayer/index.jsx
@@ -4,7 +4,7 @@ import YTApiMessages from '../../services/YTApiMessages';
 import useWindowDimensions from '../hooks/windowDimensions';
 import useOnMount from '../hooks/onMount';
 
-const HighlightVideoPlayer = ({ videoInfo, shouldVideoPlay }) => {
+const HighlightVideoPlayer = ({ videoInfo, shouldVideoPlay, controls }) => {
   const videoId = videoInfo.videoId;
   const [contentWindow, setContentWindow] = useState(null);
   const { width } = useWindowDimensions();
@@ -32,7 +32,7 @@ const HighlightVideoPlayer = ({ videoInfo, shouldVideoPlay }) => {
         title="highlight-player"
         id="highlight-player"
         src={`https://www.youtube.com/embed/${videoId}?enablejsapi=1&autoplay=1&start=0&disablekb=0&rel=0&modestbranding=1&autohide=1&controls=${
-          (width <= 1024 && '1') || '0'
+          ((width <= 1024 || controls) && '1') || '0'
         }&playerapiid=ytplayer`}
         frameBorder="0"
         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen"
